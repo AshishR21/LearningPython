930b94ddfb8fe3845ff150d9c6ba17d48378b7e9
Ashish Ranjan <ash.ran.ranjan1122@gmail.com>
2019-10-20 23:32:09 +0530

    Now, All Arthematic Functions should be called through Calculate Class A New Function "Comapre" added Day1 file removed and these are mentioned seperately in future commits

--- Basic Arthematic Fuctions.py
+++ Basic Arthematic Fuctions.py
@@ -5,18 +5,34 @@ Created on Thu Mar  7 22:39:50 2019
 @author: Ashu
 """
 
-def add(x,y):
-    return x+y
 
-def subtract(x,y):
-    return x-y
+class Calculate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-def divison(x,y):
-    return x/y
+    def add(x, y):
+        return x + y
 
-def multiplication(x,y):
-    return x*y
+    def subtract(x, y):
+        return x - y
 
-def power(x,y):
-    return x^y
+    def divide(x, y):
+        return x / y
 
+    def multiply(x, y):
+        return x * y
+
+    def power(x, y):
+        return x ^ y
+
+    def compare(x,y):
+        if x == y:
+            print("Matching : True")
+        else:
+            print("Matching : False")
+"""
+Calling Function
+"""
+
+Calculate.compare(10,10)
